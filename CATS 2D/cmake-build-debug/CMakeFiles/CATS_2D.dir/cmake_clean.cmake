f353abb16d4e35bab45e0df88a97a2de1d21bd46
Anna <anuta.denisovna@gmail.com>
2020-01-30 12:42:29 +1000

    2D

---
+++ CATS 2D/cmake-build-debug/CMakeFiles/CATS_2D.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CATS_2D.dir/main.cpp.obj"
+  "CATS_2D.pdb"
+  "CATS_2D.exe"
+  "CATS_2D.exe.manifest"
+  "libCATS_2D.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CATS_2D.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
