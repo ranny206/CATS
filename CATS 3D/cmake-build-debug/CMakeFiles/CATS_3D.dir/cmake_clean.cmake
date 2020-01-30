961a70f45892edb10ae9aa9d5b895190ce4bccf0
Anna <anuta.denisovna@gmail.com>
2020-01-30 12:43:04 +1000

    3D

---
+++ CATS 3D/cmake-build-debug/CMakeFiles/CATS_3D.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CATS_3D.exe"
+  "CATS_3D.exe.manifest"
+  "CATS_3D.pdb"
+  "CMakeFiles/CATS_3D.dir/main.cpp.obj"
+  "libCATS_3D.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CATS_3D.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
